f3ae7011473d8283d589f0d4ef4dd46e9641e327
watermelon <zfquan91@foxmail.com>
2022-07-19 20:28:00 +0800

    feat: sh

---
+++ sh/prod_sh/云效/yunxiao_sh.sh
@@ -0,0 +1,3 @@
+mkdir -p /home/lhs/cutepet/app/cutepet-system
+tar -zxf /home/lhs/cutepet/package/cutepet-system.tgz -C /home/lhs/cutepet/app/cutepet-system
+sh /home/lhs/cutepet/app/cutepet-system/sh/deploy.sh restart
